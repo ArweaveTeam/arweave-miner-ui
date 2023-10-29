0ea5e8ecd416642ae65a62bb3d2d6674e3fce8e9
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-10-29 20:45:40 +0500

    homepage added

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { MainLayout } from "../layouts";
+import { useRouter } from "next/router";
 
 export default function HomePage() {
+  const router = useRouter();
+
   const [message, setMessage] = React.useState("No message found");
 
   React.useEffect(() => {
@@ -11,9 +14,25 @@ export default function HomePage() {
     });
   }, []);
 
+  const handleConnectDashboardBtn = () => {
+    router.push("/dashboard");
+  }
+
   return (
     <MainLayout>
-      <h1 className="text-3xl font-semibold">Homepage screen</h1>
+      <div className="w-full">
+        <div className="max-w-screen-xl mx-auto p-4 h-[90vh]">
+          <div className="flex flex-col items-start justify-center h-full">
+            <h1 className="text-3xl font-bold mb-2">Arweave Mining Dashboard.</h1>
+            <p className="font-light mt-2">Connect to view your stats of your experience mining.</p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <button type="button" onClick={handleConnectDashboardBtn} className="text-white px-6 py-3 rounded-md mt-4 bg-[#494949] uppercase w-64 hover:bg-[#494949]/80">Connect to Dashboard</button>
+              <button type="button" className="text-[#494949] px-6 py-3 rounded-md mt-4 uppercase border border-[#494949] w-64 hover:bg-gray-200">Learn</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </MainLayout>
   );
 }
--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     './renderer/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      // use colors only specified
-      white: colors.white,
-      gray: colors.gray,
-      blue: colors.blue,
-    },
+    // colors: {
+    //   // use colors only specified
+    //   white: colors.white,
+    //   gray: colors.gray,
+    //   blue: colors.blue,
+    // },
     extend: {},
   },
   plugins: [],
