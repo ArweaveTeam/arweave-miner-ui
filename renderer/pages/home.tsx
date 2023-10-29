f2f45e8a3c6628d8c44047099718006712aff3bb
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-10-30 03:04:22 +0500

    homepage updated

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,14 +5,14 @@ import { useRouter } from "next/router";
 export default function HomePage() {
   const router = useRouter();
 
-  const [message, setMessage] = React.useState("No message found");
+  // const [message, setMessage] = React.useState("No message found");
 
-  React.useEffect(() => {
-    window.ipc?.on("message", (message: string) => {
-      setMessage(message);
-      console.log(message);
-    });
-  }, []);
+  // React.useEffect(() => {
+  //   window.ipc?.on("message", (message: string) => {
+  //     setMessage(message);
+  //     console.log(message);
+  //   });
+  // }, []);
 
   const handleConnectDashboardBtn = () => {
     router.push("/dashboard");
@@ -23,8 +23,8 @@ export default function HomePage() {
       <div className="w-full">
         <div className="max-w-screen-xl mx-auto p-4 h-[90vh]">
           <div className="flex flex-col items-start justify-center h-full">
-            <h1 className="text-3xl font-bold mb-2">Arweave Mining Dashboard.</h1>
-            <p className="font-light mt-2">Connect to view your stats of your experience mining.</p>
+            <h1 className="text-3xl font-medium mb-2 tracking-wide">Arweave Mining Dashboard</h1>
+            <p className="font-extralight mt-2 tracking-wider">Connect to view your stats of your experience mining.</p>
 
             <div className="flex items-center gap-4 mt-6">
               <button type="button" onClick={handleConnectDashboardBtn} className="text-white px-6 py-3 rounded-md mt-4 bg-[#494949] uppercase w-64 hover:bg-[#494949]/80">Connect to Dashboard</button>
