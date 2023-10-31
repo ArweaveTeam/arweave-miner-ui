2647cd002481cd21e74b8a395a8071d0277f148c
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-10-31 17:04:17 +0500

    extra reducer removed
    its depreciated

--- renderer/store/minorSlice.tsx
+++ renderer/store/minorSlice.tsx
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { AppState } from "./index";
-import { HYDRATE } from "next-redux-wrapper";
 
 // Type for our state
 export interface MinorState {
@@ -24,17 +23,6 @@ export const minorSlice = createSlice({
             state.data_packaged = action.payload.data_packaged;
             state.hash_rate = action.payload.hash_rate;
             state.earnings = action.payload.earnings;
-        },
-    },
-
-    extraReducers: {
-        [HYDRATE]: (state, action) => {
-            return {
-                ...state,
-                ...action.payload.data_packaged,
-                ...action.payload.hash_rate,
-                ...action.payload.earnings,
-            };
         },
     },
 });
