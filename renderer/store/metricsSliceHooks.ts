db30386c2bd7b709bc4e5b9474b4a3cd9b3036c3
Hlöðver Sigurðsson <hlolli@gmail.com>
2023-11-17 14:46:13 +0100

    feat: optimise selectors equalify-fn

--- renderer/store/metricsSliceHooks.ts
+++ renderer/store/metricsSliceHooks.ts
@@ -9,25 +9,37 @@ import {
 } from "./selectors";
 
 export const useHashRate = () => {
-  return useSelector(selectHashRate);
+  return useSelector(selectHashRate, (prev, current) => {
+    return prev.hashRate === current.hashRate;
+  });
 };
 
 export const useEarnings = () => {
-  return useSelector(selectEarnings);
+  return useSelector(selectEarnings, (prev, current) => {
+    return prev.earnings === current.earnings;
+  });
 };
 
 export const useDataPacked = () => {
-  return useSelector(selectDataPacked);
+  return useSelector(selectDataPacked, (prev, current) => {
+    return prev.dataPacked === current.dataPacked;
+  });
 };
 
 export const useDataUnpacked = () => {
-  return useSelector(selectDataUnpacked);
+  return useSelector(selectDataUnpacked, (prev, current) => {
+    return prev.dataUnpacked === current.dataUnpacked;
+  });
 };
 
 export const useStorageAvailable = () => {
-  return useSelector(selectStorageAvailable);
+  return useSelector(selectStorageAvailable, (prev, current) => {
+    return prev.storageAvailable === current.storageAvailable;
+  });
 };
 
 export const useWeaveSize = () => {
-  return useSelector(selectWeaveSize);
+  return useSelector(selectWeaveSize, (prev, current) => {
+    return prev.weaveSize === current.weaveSize;
+  });
 };
