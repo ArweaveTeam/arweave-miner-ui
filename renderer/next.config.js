d27f4147f2413a05a0e3b606eee9e128850dd873
Hlöðver Sigurðsson <hlolli@gmail.com>
2023-11-29 16:54:19 +0100

    feat: headless standalone nextjs app

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isHeadless = process.env.AR_MINER_UI_HEADLESS === "true";
+const maybeHeadlessConfig = isHeadless
+  ? {
+      output: "standalone",
+      distDir: "dist-headless",
+    }
+  : {};
+
 module.exports = {
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  ...maybeHeadlessConfig,
   webpack: (config) => {
     return config;
   },
